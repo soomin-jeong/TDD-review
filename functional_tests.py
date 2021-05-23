cc9faedf658b72d56ffc5ac949cdbb6c50a22ffe
Soo Min Jeong <soominjeongkr@gmail.com>
2021-05-23 15:59:40 +0200

    refactored the checking the item in the list

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_test: str) -> None:
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        )
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(url='http://127.0.0.1:8000/')
 
@@ -35,17 +43,19 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # TODO: There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
         #  (Edith is very methodical)
         # TODO: The page updates again, and now shows both items on her list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
         self.fail('Finish the test!')
+
 
         # TODO: Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL
         #  for her -- there is some explanatory text to that effect.
