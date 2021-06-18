09e38bd4c7d870b0bb273726dc4bda6167172e2d
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-18 11:18:02 +0200

    fixed functional test failures

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -107,10 +107,24 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
 
