f821bf508d080ab8d65006a8f4a5255bb7ad63ec
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-19 13:45:37 +0200

    testing python version

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -6,8 +6,8 @@ REPO_URL = 'https://github.com/soomin-jeong/TDD-review.git'
 
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
+    site_folder = '/home/{}/sites/{}'.format(env.user, env.host)
+    run('mkdir -p {}'.format(site_folder))
     with cd(site_folder):
         _get_latest_source()
         _update_virtualenv()
@@ -20,26 +20,27 @@ def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run(f'git clone {REPO_URL}')
+        run('git clone {}'.format(REPO_URL))
     current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f'git reset --hard {current_commit}')
+    run('git reset --hard {}'.format(current_commit))
 
 
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
-        run(f'python3.6 -m venv virtualenv')
+        run('python3.6 -m venv virtualenv')
     run('./virtualenv/bin/pip install -r requirements.txt')
 
 
 def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={env.host}')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    append('.env', 'SITENAME={}'.format(current_commit))
     current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        append('.env', 'DJANGO_SECRET_KEY={}'.format(new_secret))
 
 
 def _update_static_files():
