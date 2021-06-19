152b9829bf0e83a3c2d2d0f40a3819e8f64e73dc
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-19 10:59:07 +0200

    for geckodriver

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,7 +11,7 @@ MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
             self.live_server_url = 'http://' + staging_server
