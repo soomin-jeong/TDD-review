653bea25c3dbf1cd45e494c174f39240aeb1d643
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-19 13:33:04 +0200

    fixed a typo

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
-REPO_URL = 'https://github.com/soomin-jeong/TDD-review'
+REPO_URL = 'https://github.com/soomin-jeong/TDD-review.git'
 
 
 def deploy():
