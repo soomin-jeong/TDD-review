397dcfc98741989b3ac6b583fae968bab49e411e
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-20 10:37:45 +0200

    adjus tnew list view to do model validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
 
@@ -30,6 +30,13 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'The second list item')
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
 
 
 class NewListTest(TestCase):
@@ -77,6 +84,8 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
 
 
 
