829966d7be691254998e469707169cd312698880
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-20 11:16:31 +0200

    fixed a test

--- lists/views.py
+++ lists/views.py
@@ -25,16 +25,16 @@ def new_list(request):
         list_.delete()
         error_msg = "You can't have an empty list item"
         return render(request, 'home.html', {'error': error_msg})
-    return redirect(list_)
+    return redirect('view-list', list_.id)
 
 
 def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
     try:
-        list_ = List.objects.get(id=list_id)
         item = Item.objects.create(text=request.POST['item_text'], list=list_)
         item.full_clean()
         item.save()
         return redirect(list_)
     except ValidationError:
         error_msg = "You can't have an empty list item"
-        return render(request, 'home.html', {'error': error_msg})
+        return render(request, 'list.html', {'list': list_, 'error': error_msg})
