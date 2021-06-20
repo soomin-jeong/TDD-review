069921f4367c8804578fcdcf06a6e358208dc49b
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-20 09:57:56 +0200

    split out unit tests into two files

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,17 +1,6 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
+
 from lists.models import Item, List
-
-
-class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_uses_homepage_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
 
 
 class ListAndItemModelsTest(TestCase):
@@ -41,36 +30,6 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'The second list item')
         self.assertEqual(second_saved_item.list, list_)
-
-
-class ListViewTest(TestCase):
-
-    def test_uses_list_template(self):
-        list_ = List.objects.create()
-        response = self.client.get(f'/lists/{list_.id}/')
-        self.assertTemplateUsed(response, 'list.html')
-
-    def test_displays_only_items_for_that_list(self):
-        correct_list = List.objects.create()
-        Item.objects.create(text='Itemey 1', list=correct_list)
-        Item.objects.create(text='Itemey 2', list=correct_list)
-
-        other_list = List.objects.create()
-        Item.objects.create(text='Other Itemey 1', list=other_list)
-        Item.objects.create(text='Other Itemey 2', list=other_list)
-
-        response = self.client.get(f'/lists/{correct_list.id}/')
-
-        self.assertContains(response, 'Itemey 1')
-        self.assertContains(response, 'Itemey 2')
-        self.assertNotContains(response, 'Other Itemey 1')
-        self.assertNotContains(response, 'Other Itemey 2')
-
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get(f'/lists/{correct_list.id}/')
-        self.assertEqual(response.context['list'], correct_list)
 
 
 class NewListTest(TestCase):
