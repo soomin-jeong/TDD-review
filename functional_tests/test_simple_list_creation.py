c0d18f7a5662a33e06dbe9263da484cca1ce33c3
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-20 09:37:54 +0200

    divided one FT into several files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,38 +1,13 @@
 import time
-import os
-from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.options import Options
-
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 
-MAX_WAIT = 10
+from functional_tests.base import FunctionalTest
+from unittest import skip
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_test: str) -> None:
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_test, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -42,35 +17,31 @@ class NewVisitorTest(StaticLiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # TODO: She is invited to enter a to-do item straight away
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # TODO: She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
         time.sleep(1)
 
-        # TODO: When she hits enter, the page updates, and now the page lists "1: Buy peacock feathers" as an item in a to-do list"
+        # When she hits enter, the page updates, and now the page lists "1: Buy peacock feathers" as an item in a to-do list"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-        # TODO: There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
+        # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
         #  (Edith is very methodical)
-        # TODO: The page updates again, and now shows both items on her list
+        # The page updates again, and now shows both items on her list
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
-
-        self.fail('Finish the test!')
-
-        # TODO: She visits that URL - her to-do list is still there.
 
     def test_can_start_a_list_per_user(self):
         # Edith starts a new to-do list
@@ -130,18 +101,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-
