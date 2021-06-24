83a9b60b25f264b638ce5d1f91d4406cfdd87466
Soo Min Jeong <soominjeongkr@gmail.com>
2021-06-24 19:40:38 +0200

    added login and out

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.contrib.auth import logout
 from accounts import views
 
 app_name = 'accounts'
@@ -7,5 +8,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send-login-email'),
     path('login', views.login, name='login'),
+    path('logout', logout, {'next_page': '/'}, name='logout')
 
 ]
